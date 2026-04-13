e4511bf038e67240c96acade2fe2be06a6cd8080
dentheroblox <eduverso4@gmail.com>
2026-04-13 22:29:20 +0800

    change

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ app.use(cors({
   },
   credentials: true
 }));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
